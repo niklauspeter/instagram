134bcaa33c04105741ba815426bec0c30035b02d
klaus <oriokiklaus@gmail.com>
2019-05-21 16:27:39 +0300

    define route for static files and template context processors to store uploaded images

--- insta/views.py
+++ insta/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse
-
+from .models import Image
 
 def welcome(request):
-    return render(request, 'all-posts/index.html')
+    image = Image.display_images()
+    return render(request, 'all-posts/index.html', {"image":image})
 
 # Create your views here.
