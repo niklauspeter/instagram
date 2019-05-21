b1ebe5229ad03e7b317034b6b676dc426a600c4e
klaus <oriokiklaus@gmail.com>
2019-05-21 14:19:45 +0300

    add an image model with a one to many relationship with the user

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField()
+    image_name= models.CharField(max_length= 30)
+    image_caption= models.CharField(max_length= 30)
+    # profile = models.ForeignKey(Profile)
+    # likes =
+    # comments
+    pub_date = models.DateTimeField(auto_now_add=True)
