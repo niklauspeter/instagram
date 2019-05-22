05d2941fdcd7f5ad0c9737b69932df6ce89e36ec
klaus <oriokiklaus@gmail.com>
2019-05-22 12:49:15 +0300

    add view function to render the post form and redirect to home page if form is valid

--- insta/views.py
+++ insta/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http  import HttpResponse
 from .models import Image
 from django.contrib.auth.decorators import login_required 
+from .forms import NewImageForm, SignupForm
 
 @login_required(login_url='/accounts/login/')
 def welcome(request):
@@ -21,3 +22,18 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'all-posts/search.html',{"message":message})
+
+
+@login_required(login_url='/accounts/login/')
+def new_image(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            image.save()
+        return redirect('welcome')
+    else:
+        form = NewImageForm()
+    return render(request, 'all-posts/new_image.html', {"form": form})
--- instagram/settings.py
+++ instagram/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'insta.apps.InstaConfig',
     'bootstrap3',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
