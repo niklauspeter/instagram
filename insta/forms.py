a2c6ab6c5fafbb02516becdd13650d21dab04d9f
klaus <oriokiklaus@gmail.com>
2019-05-22 16:52:43 +0300

    add form to aquire profile input from models

--- insta/forms.py
+++ insta/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Image
+from .models import Image, Profile
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
@@ -16,3 +16,9 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['username']
