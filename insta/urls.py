343364f056cd34d65d3f4ba34befec9c3e5b5ad4
klaus <oriokiklaus@gmail.com>
2019-05-23 16:55:03 +0300

    add urlconfig for the proile page

--- insta/urls.py
+++ insta/urls.py
@@ -8,8 +8,10 @@ urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^new/image$', views.new_image, name='new_image'),
-    url(r'^edit/profile$',views.edit_profile, name='edit-profile'),
-     url(r'^profile/',views.profile, name='profile'),
+    url(r'^edit/profile',views.edit_profile, name='edit-profile'),
+    url(r'^profile/',views.profile, name='profile'),
+    url(r'^look/',views.look, name='look'),
+    
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
